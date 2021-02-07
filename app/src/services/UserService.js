c634fe8c56d0e3b42f306f1fa1a1744e47257af1
Irena Gao <irenagao2013@gmail.com>
2021-02-07 04:45:04 -0500

    Update UserService.js

--- app/src/services/UserService.js
+++ app/src/services/UserService.js
@@ -1,10 +1,18 @@
+export async function getUserInfo() {
+
+    const response = await fetch('/getresult', {
+        headers: {'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+    }
+    });
+    return await response;
+}
+
 export async function createUser(data) {
     const response = await fetch(`/result`, {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({user: data})
       })
-      console.log("createuser");
-      console.log(data);
-    return await response.json();
+    return response;
 }
