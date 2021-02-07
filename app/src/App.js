d4e904cbcc4dce1f2f487cfa96ba6d0380dad135
Irena Gao <irenagao2013@gmail.com>
2021-02-07 04:45:30 -0500

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,25 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Quiz from './screens/Quiz';
+import ChatRecs from './screens/ChatRecs';
+import Home from './screens/Home';
+import Logo from './assets/softshelllogo.png';
+import background from './assets/background.png';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundImage: `url(${background})`, height: '100%', margin: 0}}>
       <Router>
-        {/* <img src={Logo} style={{width: '15%', margin: '3%', marginBottom: '-2%'}} /> */}
+        <div style={{width: '100%', backgroundColor: '#25283C', padding: '0.5%'}}>
+          <img src={Logo} style={{width: '5%', marginLeft: '2%'}} />
+          <p style={{color: "white", height: '', fontSize: '30px', marginTop: '-4%', fontFamily: 'Arial-Black', marginLeft: '8.5%'}}>SoftShell</p>
+        </div>
         <Route path="/Quiz" exact render={() => <Quiz />} />
+        <Route path="/ChatRecs" exact render={() => <ChatRecs />} />
+        <Route path="/" exact render={() => <Home />} />
       </Router>
     </div>
   );
 }
 
 export default App;
+
